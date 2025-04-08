d8dd24286c52fd1506c1cb601b037b1a5b9227de
OttoCollanErixon <ottcol258@edu.linkoping.se>
2025-04-08 09:01:19 +0000

    asdghgjk

---
+++ Slutprojekt/main.js
@@ -0,0 +1,15 @@
+// const btnOpenMenu = document.querySelector("#menu-open");
+// const btnClosedMenu = document.querySelector("#menu-close");
+// const menu = document.querySelector(".menu");
+
+// function openMenu(event) {
+//     menu.style.display = "flex";
+//     btnOpenMenu.style.display = "none";
+// }
+// btnOpenMenu.addEventListener("click", openMenu);
+
+// function closedMenu(event) {
+//     menu.style.display = "none";
+//     btnOpenMenu.style.display = "block";
+// }
+// btnOpenMenu.addEventListener("click", closedMenu);
