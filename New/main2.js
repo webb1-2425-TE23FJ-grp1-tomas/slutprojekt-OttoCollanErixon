2323c9102fb90127df00d001b907c43c9c9b924a
OttoCollanErixon <ottcol258@edu.linkoping.se>
2025-05-14 08:57:31 +0000

    fg

--- New/main2.js
+++ New/main2.js
@@ -1,15 +1,18 @@
 const btnOpenMenu = document.getElementById("menu-open");
 const btnCloseMenu = document.getElementById("menu-close");
 const menu = document.querySelector(".menu");
+const searchbar = document.getElementById("searchbar");
 
 function openMenu(event) {
     menu.style.display = "flex";
     btnOpenMenu.style.display = "none";
+    searchbar.style.display= "none";
 }
 btnOpenMenu.addEventListener("click", openMenu);
 
 function closeMenu(event) {
     menu.style.display = "none";
     btnOpenMenu.style.display = "block";
+    searchbar.style.display = "block";
 }
 btnCloseMenu.addEventListener("click", closeMenu);
