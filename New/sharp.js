09bd5a3b4a0d0c6abd61c9bfac1095875d5799a0
OttoCollanErixon <ottcol258@edu.linkoping.se>
2025-05-26 12:48:45 +0000

    nya optade bilder

--- New/sharp.js
+++ New/sharp.js
@@ -9,12 +9,14 @@ for (file of files) {
     let filename = file.slice(0, file.indexOf("."));
 
     sharp(`${assetsDir}/${file}`)
+        .resize({ width: 400 })
         .webp({quality:60})
-        .toFile(`${outputDir}/${filename}-60.webp`)
+        .toFile(`${outputDir}/${filename}-400.webp`)
     
     sharp(`${assetsDir}/${file}`)
+        .resize({ width: 650})
         .webp({quality:80})
-        .toFile(`${outputDir}/${filename}-80.webp`)
+        .toFile(`${outputDir}/${filename}-650.webp`)
 }
 
 // sharp('Bilder/Kladesplagg/ValstarJacka.webp')
